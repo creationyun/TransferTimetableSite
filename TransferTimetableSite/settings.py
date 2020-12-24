2d20322c5121e8962700074e62e8381a4671c8ec
Creation Yun <36063812+creationyun@users.noreply.github.com>
2020-12-25 01:07:19 +0900

    Update settings.py to update STATIC_URL

--- TransferTimetableSite/settings.py
+++ TransferTimetableSite/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/timetable/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
