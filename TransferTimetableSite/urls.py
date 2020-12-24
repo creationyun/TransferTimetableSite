0a21ca339c388a48b6ce59a6302eee4798604ea0
Creation Yun <36063812+creationyun@users.noreply.github.com>
2020-12-25 01:04:50 +0900

    Update urls.py adding 'timetable' dir

--- TransferTimetableSite/urls.py
+++ TransferTimetableSite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('timetable.urls')),
-    path('admin/', admin.site.urls),
+    path('timetable/', include('timetable.urls')),
+    path('timetable/admin/', admin.site.urls),
 ]
