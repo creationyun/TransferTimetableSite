4ecf9a35b80aa52e5e646b2dd50c2004873b2e5c
Creation Yun <36063812+creationyun@users.noreply.github.com>
2020-12-25 01:01:53 +0900

    Update settings.py to upload allowed host

--- TransferTimetableSite/settings.py
+++ TransferTimetableSite/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = secrets['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '.midnightguidetohome.info'
+]
 
 
 # Application definition
